43298a0a4fed4ae6d59149abd93b4363516863ce
MHOBLACK <chatchaichosawat@gmail.com>
2023-02-08 20:37:09 +0700

    add condition

--- db/check_user.php
+++ db/check_user.php
@@ -5,22 +5,29 @@
     if (isset($_SESSION['username'])) {
 
         $username = $_SESSION['username'];
-        $market_name = $_GET['market_name'];
 
-        $show_market = "SELECT * FROM market_list WHERE market_name = '$market_name'";
-        $show_market_query = mysqli_query($conn, $show_market);
-        $show_market_result = mysqli_fetch_assoc($show_market_query);
+        $check_user = "SELECT * FROM user WHERE username = '$username'";
+        $check_user_query = mysqli_query($conn, $check_user);
+        $check_user_result = mysqli_fetch_assoc($check_user_query);
 
-        $owner = $show_market_result['owner'];
+        if (isset($_GET['market_name'])) {
 
-        if ($username === $owner) {
+            $market_name = $_GET['market_name'];
 
-            // ไม่ต้องทำอะไร
-            
-        } else {
-            header('location: index.php');
+            $show_market = "SELECT * FROM market_list WHERE market_name = '$market_name'";
+            $show_market_query = mysqli_query($conn, $show_market);
+            $show_market_result = mysqli_fetch_assoc($show_market_query);
+
+            $owner = $show_market_result['owner'];
+
+            if ($username === $owner) {
+    
+                // ไม่ต้องทำอะไร
+                
+            } else {
+                header('location: index.php');
+            }
         }
-
 
     } else {
         header('location: index.php');
