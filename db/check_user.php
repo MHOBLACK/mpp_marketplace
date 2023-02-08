70dc51e06db28a832bedcdb4ce28eb66d183c948
MHOBLACK <chatchaichosawat@gmail.com>
2023-02-08 18:10:44 +0700

    add condition

--- db/check_user.php
+++ db/check_user.php
@@ -3,9 +3,25 @@
     include('server.php');
 
     if (isset($_SESSION['username'])) {
-        if ($_SESSION['user_role'] !== 'admin') {
+
+        $username = $_SESSION['username'];
+        $market_name = $_GET['market_name'];
+
+        $show_market = "SELECT * FROM market_list WHERE market_name = '$market_name'";
+        $show_market_query = mysqli_query($conn, $show_market);
+        $show_market_result = mysqli_fetch_assoc($show_market_query);
+
+        $owner = $show_market_result['owner'];
+
+        if ($username === $owner) {
+
+            // ไม่ต้องทำอะไร
+            
+        } else {
             header('location: index.php');
         }
+
+
     } else {
         header('location: index.php');
     }
